be9656dfb4eb5d8917fc1f4c43099521d788d9a4
divyanshu22299 <divyanshusingh2229@gmail.com>
2026-03-19 12:03:42 +0530

    fix ignore

--- frontend/.next/dev/server/chunks/ssr/website_frontend__next-internal_server_app_page_actions_5b84f34e.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/website/frontend/.next-internal/server/app/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=website_frontend__next-internal_server_app_page_actions_5b84f34e.js.map
--- frontend/.next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"BanhKKiwMuCDUvdo4Kuoe+Kq6KC1ZckFGxKIl+Imemw=\"\n}"
